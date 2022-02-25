ff4e5e55e9595d95bd0051979a27fa6e2381dbc0
Annaliskirwa <annaliskirwa@gmail.com>
2022-02-25 14:45:25 +0300

    Injects the auth service into the component

--- angular-auth/src/app/app.component.ts
+++ angular-auth/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
+// IMPORT THE AUTHSERVICE FROM THE SDK
+import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<button (click)="auth.loginWithRedirect()">Log in</button>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-auth';
+
+  // Inject teh auth service into the component through thr constructor
+  constructor(
+    public auth: AuthService
+  ){}
 }
