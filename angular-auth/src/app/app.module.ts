6d2d9d783e225700075c564c3ed65218102366bc
Annaliskirwa <annaliskirwa@gmail.com>
2022-02-25 14:23:43 +0300

    importing the module into the application

--- angular-auth/src/app/app.module.ts
+++ angular-auth/src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+// import the auth module fomr the sdk
+import { AuthModule } from '@auth0/auth0-angular';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // import the module into the application
+    AuthModule.forRoot({
+      domain: 'dev-zuzrkzsf.us.auth0.com',
+      clientId: '5rvKpFd6SH5frjiiPDuYrMcnprDvnlPM'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
